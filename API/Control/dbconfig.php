9365de5ba5c0ab0a6fabff2a8341dcd58f507287
Nyman <sikazwe7@gmail.com>
2019-02-22 18:36:40 -0600

    DB_config

--- API/Control/dbconfig.php
+++ API/Control/dbconfig.php
@@ -5,9 +5,9 @@
 /****************************************************/
 /****************************************************/ 
  
-define('DB_USER', "id7086738_sikalabs");     // Your database user name
-define('DB_PASSWORD', "sikalabs@website");			// Your database password (mention your db password here)
-define('DB_DATABASE', "id7086738_schedule_apis"); // Your database name
+define('DB_USER', "Your_database_username");     // Your database user name
+define('DB_PASSWORD', "Your_db_user_password");			// Your database password (mention your db password here)
+define('DB_DATABASE', "Your_DB_name"); // Your database name
 define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
  
 ?>
